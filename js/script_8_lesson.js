77e50041d47836e8bcc4c2d8ee05d7ad6957d646
Igor <igor.turkin@mail.ru>
2020-06-24 21:45:25 +0600

    first commit

---
+++ js/script_8_lesson.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`); // Интерполяция сработает только при косых кавычках(Бэктики)
+
+
+const name = "Chaser"
+alert(`Хочу ${name}`);
